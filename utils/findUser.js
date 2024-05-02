f3b4e453a7265a028220f21377daa4d2788267a1
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-05-02 13:16:54 +0200

    Normalize username within findUser

--- utils/findUser.js
+++ utils/findUser.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 
-async function findUser(normalizedUsername) {
+async function findUser(username) {
+  const normalizedUsername = username.toLowerCase();
   const currentUser = await User.findOne({ normalizedUsername }).exec();
 
   if (!currentUser) return false;
